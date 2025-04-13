7967305764717e77eb661d180c1601f89a60c6e3
kgh990910 <kgh990910@gmail.com>
2025-04-13 14:07:21 +0900

    Fix 06.sh

--- scripts/nmservice/06_wifi_monitor.sh
+++ scripts/nmservice/06_wifi_monitor.sh
@@ -5,7 +5,8 @@ INTERFACE="wlan0"
 while true; do
   STATE=$(nmcli -t -f GENERAL.STATE device show $INTERFACE | cut -d: -f2 | cut -d' ' -f1)
 
-  if [[ "$STATE" -lt 70 ]]; then
+  MODE=$(iw dev $INTERFACE info | grep type | awk '{print $2}')
+  if [[ "$STATE" -lt 70 && "$MODE" != "AP" ]]; then
     echo "⚠️ Wi-Fi disconnected. Switching mode..."
     /home/$USER/AutoFormBotCode/scripts/nmservice/04_auto_wifi_or_ap.sh
   fi
