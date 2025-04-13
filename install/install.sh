e0d2042e57afd83b0c26da49f12d33f0849fe6ac
kgh990910 <kgh990910@gmail.com>
2025-04-13 16:04:01 +0900

    Fix install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ echo "AutoFormBot Auto Wi-Fi/AP mode switching script install..."
 
 AUTOFORM_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-bash "$AUTOFORM_PATH/AutoFormBotCode/scripts/nmservice/01_setup_web_server.sh"
+bash "$AUTOFORM_PATH/scripts/nmservice/01_setup_web_server.sh"
 bash "$AUTOFORM_PATH/scripts/nmservice/05_enable_auto_wifi_service.sh"
 bash "$AUTOFORM_PATH/scripts/nmservice/07_enable_wifi_monitor.sh"
 
