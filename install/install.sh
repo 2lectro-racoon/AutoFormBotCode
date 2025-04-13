5c170524e3c2c1e02699abd7ef41bc9025ca168f
kgh990910 <kgh990910@gmail.com>
2025-04-13 15:54:40 +0900

    Auto Switching AP/STA Done, Add Total install script

---
+++ install/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "AutoFormBot Auto Wi-Fi/AP mode switching script install..."
+
+AUTOFORM_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+
+bash "$AUTOFORM_PATH/scripts/nmservice/01_setup_web_server.sh"
+bash "$AUTOFORM_PATH/scripts/nmservice/05_enable_auto_wifi_service.sh"
+bash "$AUTOFORM_PATH/scripts/nmservice/07_enable_wifi_monitor.sh"
+
+echo "AutoFormBot Auto Wi-Fi/AP mode switching script installed..."
