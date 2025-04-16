eaa5a59dc408ce8e8a5371ef36133e28be823b0b
kgh990910 <kgh990910@gmail.com>
2025-04-16 13:34:41 +0900

    Fix PYTHONPATH

--- install/install.sh
+++ install/install.sh
@@ -18,9 +18,9 @@ bash "$AUTOFORM_PATH/scripts/YOLOv11/01_yolo_setup.sh"
 bash "$AUTOFORM_PATH/scripts/tflite/01_tflite_setup.sh"
 bash "$AUTOFORM_PATH/scripts/gpio/02_gpio_setup.sh"
 
-echo '# === Auto PYTHONPATH for AutoFormBotCode ===' >> AutoFormBot_venv/bin/activate
-echo 'export AUTOBOT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ && pwd)"' >> AutoFormBot_venv/bin/activate
-echo 'export PYTHONPATH="$AUTOBOT_ROOT/scripts/gpio:$AUTOBOT_ROOT/scripts/opencv:$AUTOBOT_ROOT/scripts/tflite:$AUTOBOT_ROOT/scripts/YOLOv11:$PYTHONPATH"' >> AutoFormBot_venv/bin/activate
+echo '# === Auto PYTHONPATH for AutoFormBotCode ===' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
+echo 'export AUTOBOT_ROOT="/home/autoformbotpi/AutoFormBot"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
+echo 'export PYTHONPATH="$AUTOBOT_ROOT/scripts/gpio:$AUTOBOT_ROOT/scripts/opencv:$AUTOBOT_ROOT/scripts/tflite:$AUTOBOT_ROOT/scripts/YOLOv11:$PYTHONPATH"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
 
 bash "$AUTOFORM_PATH/scripts/gpio/01_servo_stop.sh"
 
