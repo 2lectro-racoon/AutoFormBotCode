10ffbea17363a7bff88088bd113f24b80ec5155e
kgh990910 <kgh990910@gmail.com>
2025-05-12 18:20:49 +0900

    Change lib-stream to picamera2

--- scripts/afb/afb.py
+++ scripts/afb/afb.py
@@ -1,39 +1,21 @@
 # afb/camera.py
 
 import cv2
-import subprocess
 import time
+from picamera2 import Picamera2
 
-_cap = None
-_proc = None
+_picam2 = None
 
-def _start_stream():
-    global _proc
-    if _proc is None:
-        _proc = subprocess.Popen([
-            "libcamera-vid",
-            "-t", "0",
-            "--width", "640",
-            "--height", "480",
-            "--framerate", "30",
-            "--listen",
-            "-o", "tcp://0.0.0.0:8888"
-        ])
-        time.sleep(2)  # Wait for the stream to be ready
-
-def _open_capture():
-    global _cap
-    if _cap is None:
-        _cap = cv2.VideoCapture("tcp://localhost:8888", cv2.CAP_FFMPEG)
-        while not _cap.isOpened():
-            time.sleep(0.1)
+def _start_camera():
+    global _picam2
+    if _picam2 is None:
+        _picam2 = Picamera2()
+        _picam2.configure(_picam2.create_preview_configuration(main={"size": (640, 480)}))
+        _picam2.start()
+        time.sleep(1)  # Allow camera to warm up
 
 def get_image():
-    global _cap
-    if _cap is None:
-        _start_stream()
-        _open_capture()
-    ret, frame = _cap.read()
-    if not ret:
-        raise RuntimeError("Failed to capture frame from camera")
-    return frame
+    global _picam2
+    if _picam2 is None:
+        _start_camera()
+    return _picam2.capture_array()
