964e228be73468c0ec6cf762007130dca54ec476
kgh990910 <kgh990910@gmail.com>
2025-05-12 18:00:27 +0900

    Add afb path(python env)

--- install/install.sh
+++ install/install.sh
@@ -21,7 +21,7 @@ bash "$AUTOFORM_PATH/scripts/gpio/02_gpio_setup.sh"
 
 echo '# === Auto PYTHONPATH for AutoFormBotCode ===' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
 echo 'export AUTOBOT_ROOT="/home/autoformbotpi/AutoFormBotCode"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
-echo 'export PYTHONPATH="$AUTOBOT_ROOT/scripts/gpio:$AUTOBOT_ROOT/scripts/opencv:$AUTOBOT_ROOT/scripts/tflite:$AUTOBOT_ROOT/scripts/YOLOv11:$PYTHONPATH"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
+echo 'export PYTHONPATH="$AUTOBOT_ROOT/scripts/gpio:$AUTOBOT_ROOT/scripts/opencv:$AUTOBOT_ROOT/scripts/tflite:$AUTOBOT_ROOT/scripts/YOLOv11:$AUTOBOT_ROOT/scripts/afb:$PYTHONPATH"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
 
 bash "$AUTOFORM_PATH/scripts/gpio/01_servo_stop.sh"
 
