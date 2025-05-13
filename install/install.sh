fe9fcb47d1daad70a6962c41b546b209b965b747
kgh990910 <kgh990910@gmail.com>
2025-05-13 12:32:24 +0900

    Refactor install.sh

--- install/install.sh
+++ install/install.sh
@@ -22,7 +22,7 @@ bash "$AUTOFORM_PATH/scripts/afb/01_picamera2_setup.sh"
 
 echo '# === Auto PYTHONPATH for AutoFormBotCode ===' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
 echo 'export AUTOBOT_ROOT="/home/autoformbotpi/AutoFormBotCode"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
-echo 'export PYTHONPATH="$AUTOBOT_ROOT/scripts/gpio:$AUTOBOT_ROOT/scripts/opencv:$AUTOBOT_ROOT/scripts/tflite:$AUTOBOT_ROOT/scripts/YOLOv11:$AUTOBOT_ROOT/scripts/afb:$PYTHONPATH"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
+echo 'export PYTHONPATH="$AUTOBOT_ROOT/scripts/gpio:$AUTOBOT_ROOT/scripts/opencv:$AUTOBOT_ROOT/scripts/tflite:$AUTOBOT_ROOT/scripts/YOLOv11:$AUTOBOT_ROOT/scripts/package:$PYTHONPATH"' >> /home/autoformbotpi/AutoFormBot_venv/bin/activate
 
 bash "$AUTOFORM_PATH/scripts/gpio/01_servo_stop.sh"
 
