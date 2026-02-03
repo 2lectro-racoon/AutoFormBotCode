0b5f80f4aeb0b024eb590a4b439dd98a34ae0407
kgh990910 <kgh990910@gmail.com>
2026-02-03 13:43:58 +0900

    Add quad stand code

--- scripts/package/afb/quad.py
+++ scripts/package/afb/quad.py
@@ -17,6 +17,8 @@ from typing import List
 
 from . import _spi_bus
 
+import time
+
 def servo(ch, angle) -> List[int]:
     return _spi_bus.servo_set(ch, angle)
 
@@ -29,6 +31,20 @@ def leg(ch, a0, a1, a2) ->  None:
 
 def legReset() -> None:
     _spi_bus.leg_set(0, 40, 0, 180)
+    time.sleep(0.5)
     _spi_bus.leg_set(1, 135, 180, 0)
+    time.sleep(0.5)
     _spi_bus.leg_set(2, 40, 0, 180)
+    time.sleep(0.5)
     _spi_bus.leg_set(3, 135, 180, 0)
+    time.sleep(0.5)
+
+def stand() -> None:
+    _spi_bus.leg_set(0, 90, 40, 80)
+    time.sleep(0.5)
+    _spi_bus.leg_set(1, 90, 140, 100)
+    time.sleep(0.5)
+    _spi_bus.leg_set(2, 90, 40, 80)
+    time.sleep(0.5)
+    _spi_bus.leg_set(3, 90, 140, 100)
+    time.sleep(0.5)
